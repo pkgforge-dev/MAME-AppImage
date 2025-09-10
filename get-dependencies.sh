050468a4cd26056810ba89e434d43db0d493181c
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-09-10 04:22:55 -0400

    typo

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -29,4 +29,4 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-opengl libxml2-mini opus-mini qt6-mini
+./get-debloated-pkgs.sh --add-opengl libxml2-mini opus-mini qt6-base-mini
