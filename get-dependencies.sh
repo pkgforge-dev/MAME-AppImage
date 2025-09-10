c84f981896b2f778342e8fe5a00044585b9c3b6a
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-09-10 08:19:31 +0000

    Add files via upload

---
+++ get-dependencies.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -ex
+
+EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+
+echo "Installing dependencies..."
+echo "---------------------------------------------------------------"
+pacman -Syu --noconfirm \
+	base-devel         \
+	cmake              \
+	ccache             \
+	curl               \
+	git                \
+	libx11             \
+	libxrandr          \
+	libxss             \
+	mame               \
+	pipewire-audio     \
+	pulseaudio         \
+	pulseaudio-alsa    \
+	sdl2               \
+	wget               \
+	xorg-server-xvfb   \
+	zlib               \
+	zsync
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
+chmod +x ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh --add-opengl libxml2-mini opus-mini qt6-mini
