9bc68ef8ea5e623ab3e255172cf9a965f3fd01b4
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-09-11 06:03:47 +0000

    ready for release (#3)

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,26 +7,30 @@ EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImage
 echo "Installing dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	base-devel         \
-	cmake              \
-	ccache             \
-	curl               \
-	git                \
-	libx11             \
-	libxrandr          \
-	libxss             \
-	mame               \
-	pipewire-audio     \
-	pulseaudio         \
-	pulseaudio-alsa    \
-	sdl2               \
-	wget               \
-	xorg-server-xvfb   \
-	zlib               \
+	base-devel        \
+	cmake             \
+	dconf             \
+	ccache            \
+	curl              \
+	git               \
+	libdecor          \
+	libx11            \
+	libxrandr         \
+	libxss            \
+	mame              \
+	pipewire-audio    \
+	pulseaudio        \
+	pulseaudio-alsa   \
+	sdl2              \
+	wget              \
+	xorg-server-xvfb  \
+	zlib              \
 	zsync
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-opengl libxml2-mini opus-mini qt6-mini
+./get-debloated-pkgs.sh --add-opengl libxml2-mini opus-mini qt6-base-mini
+
+pacman -Q mame | awk '{print $2; exit}' > ~/version
