3cf483f177f54e0069a0a1b0461907e021e274b0
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-08 01:43:57 -0400

    build MAME

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -17,7 +17,6 @@ pacman -Syu --noconfirm \
 	libx11            \
 	libxrandr         \
 	libxss            \
-	mame              \
 	pipewire-audio    \
 	pulseaudio        \
 	pulseaudio-alsa   \
@@ -31,6 +30,22 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-opengl libxml2-mini opus-mini qt6-base-mini
+./get-debloated-pkgs.sh --add-opengl libxml2-mini opus-mini qt6-base-mini gdk-pixbuf2-mini
+
+echo "Building MAME..."
+echo "---------------------------------------------------------------"
+sed -i -e 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg
+sed -i \
+	-e 's|-O2|-O3|'                              \
+	-e 's|MAKEFLAGS=.*|MAKEFLAGS="-j$(nproc)"|'  \
+	-e 's|#MAKEFLAGS|MAKEFLAGS|'                 \
+	/etc/makepkg.conf
+
+git clone --depth 1 https://gitlab.archlinux.org/archlinux/packaging/packages/mame.git ./mame && (
+	cd ./mame
+	makepkg -fs --noconfirm --skippgpcheck
+	ls -la .
+	pacman --noconfirm -U ./*.pkg.tar.*
+)
 
 pacman -Q mame | awk '{print $2; exit}' > ~/version
