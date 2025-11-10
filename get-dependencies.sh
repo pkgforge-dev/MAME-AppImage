0e364149c224658276de8904c40ecf505547e47c
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-10 17:33:49 -0400

    simplify build

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-set -ex
-ARCH="$(uname -m)"
+set -eu
 EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+PACKAGE_BUILDER="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/make-aur-package.sh"
 
 echo "Installing dependencies..."
 echo "---------------------------------------------------------------"
@@ -28,26 +28,8 @@ pacman -Syu --noconfirm \
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
-chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-common
-
-echo "Building MAME..."
-echo "---------------------------------------------------------------"
-sed -i -e 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg
-sed -i \
-	-e 's|-O2|-O3|'                              \
-	-e 's|MAKEFLAGS=.*|MAKEFLAGS="-j$(nproc)"|'  \
-	-e 's|#MAKEFLAGS|MAKEFLAGS|'                 \
-	/etc/makepkg.conf
-cat /etc/makepkg.conf
-
-git clone --depth 1 https://gitlab.archlinux.org/archlinux/packaging/packages/mame.git ./mame && (
-	cd ./mame
-	sed -i -e "s|x86_64|$ARCH|" ./PKGBUILD
-	makepkg -fs --noconfirm --skippgpcheck
-	ls -la .
-	pacman --noconfirm -U ./*.pkg.tar.*
-)
+wget --retry-connrefused --tries=30 "$PACKAGE_BUILDER" -O ./make-aur-package.sh
+chmod +x ./make-aur-package.sh
+./make-aur-package.sh mame
 
 pacman -Q mame | awk '{print $2; exit}' > ~/version
