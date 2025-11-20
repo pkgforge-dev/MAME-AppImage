734af326baa3a245145ba4313ccb9df5205ef8ca
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-20 08:09:06 -0400

    mmm

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -15,5 +15,5 @@ pacman -Syu --noconfirm \
 
 echo "Building MAME..."
 echo "---------------------------------------------------------------"
-gpg --recv-keys 79BE3E4300411886
+gpg --recv-keys C174B1018C40710E
 make-aur-package --archlinux-pkg mame
